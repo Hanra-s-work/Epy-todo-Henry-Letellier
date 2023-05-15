dd9cec77f6fcc0639658949b60c049fd7b3e2d90
Harleen <harleen.singh-kaur@epitech.eu>
2023-05-15 20:35:36 +0200

    [ADD] messages of display usser todos

--- src/config/short_or_detailed_message.js
+++ src/config/short_or_detailed_message.js
@@ -247,6 +247,24 @@ function display_todo_id(res, title = "", msg = "", token = "") {
     }
 }
 
+function put_user_id(res, title = "", msg = "", token = "") {
+    if (assets.isJSON(msg) === true) {
+        custom_or_bland_success(res, title, msg, token, custom_message);
+    } else if (msg === "No user found") {
+        pre_coded.not_found(res);
+    } else if (msg === "Unknown input") {
+        pre_coded.bad_parameters(res);
+    } else if (msg === injection.injection_message) {
+        injection_message(res, title, token);
+    } else {
+        if (custom_message === false) {
+            pre_coded.internal_server_error(res);
+        } else {
+            status_output.internal_server_error(res, { 'title': title, 'msg': msg, 'token': token });
+        }
+    }
+}
+
 module.exports = {
     custom_or_bland_success,
     error_body_message,
@@ -264,5 +282,6 @@ module.exports = {
     delete_users_id_messages,
     delete_todos_id_messages,
     display_put_todos,
-    display_todo_id
+    display_todo_id,
+    put_user_id
 }
